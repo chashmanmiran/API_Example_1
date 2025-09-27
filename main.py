a8d24466216cc44ff11939b0f0fd73f333443a5b
chashmanmiran <chashman121@gmail.com>
2025-09-27 00:48:46 -0400

    Testing SQL Parameters and database functions

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from random import randint
 from fastapi import FastAPI, HTTPException, Request, Response
 from typing import Any
+from sqlalchemy import create_engine
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
 
 app = FastAPI(root_path="/api/v1")
 
