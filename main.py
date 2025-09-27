df8d03d08c81dcb45028bd9dbfe3c4f97ef51761
chashmanmiran <chashman121@gmail.com>
2025-09-27 18:40:13 -0400

    Added basic pagination, no count of elements

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from annotated_types import T
 from fastapi.concurrency import asynccontextmanager
 from pydantic import BaseModel
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, func
 from typing_extensions import Annotated
-from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi import Depends, FastAPI, HTTPException, Query, Request
 from typing import Any, Generic, TypeVar
 from sqlmodel import SQLModel, create_engine
 from sqlmodel import SQLModel, Session, select
 from typing import Optional
 
 class Game(SQLModel, table=True):
-    games_id: Optional[int] = Field(default=None, primary_key=True)
+    games_id: int | None = Field(default=None, primary_key=True)
     title: Optional[str] = Field(default=None, index=True, nullable=True)
     genre: Optional[str] = Field(default=None, index=True, nullable=True)
     platform: Optional[str] = Field(default=None, index=True, nullable=True)
@@ -120,11 +120,33 @@ class GameCreate(SQLModel):
     rating: str
     description: str
     cover_image_url: str
+
+
+class PaginatedResponse(BaseModel, Generic[T]):
+    data: T
+    next: Optional[str]
+    previous: Optional[str]
     
-@app.get("/games", response_model = ApiResponse[list[Game]])
-async def read_games(session: SessionDep):
-    data = session.exec(select(Game)).all()
-    return {"data": data}
+@app.get("/games", response_model = PaginatedResponse[list[Game]])
+async def read_games(request: Request, session: SessionDep, page: int = Query(1, ge=1), page_size: int = Query(20, ge=1)):
+    limit = page_size
+    offset = (page-1)*limit
+    data = session.exec(select(Game).order_by(Game.games_id).offset(offset).limit(limit)).all()
+
+    base_url = str(request.url).split('?')[0]
+
+    next_url = f"{base_url}?page={page+1}&page_size={limit}"
+
+    if page > 1:
+        prev_url = f"{base_url}?page={page-1}&page_size={limit}"
+    else:
+        prev_url = None
+
+    return {
+        "next": next_url,
+        "previous": prev_url,
+        "data": data
+    }
 
 @app.get("/games/{id}", response_model=ApiResponse[Game])
 async def read_game(id: int, session: SessionDep):
